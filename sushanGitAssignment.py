2a901d8da1b9e7b5bbe59da0a44935bae182c4ee
fusesushan <sushan.kattel@fusemachines.com>
2023-08-03 13:26:54 +0545

    Adding precommit hooks plugin

--- sushanGitAssignment.py
+++ sushanGitAssignment.py
@@ -1,17 +1,21 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
+
 
 def divide(x, y):
     if y != 0:
         return x / y
     else:
         raise ValueError("Cannot divide by zero")
+
 
 def main():
     print("Choose what you want to do:")
@@ -20,22 +24,22 @@ def main():
     print("3. Multiplication")
     print("4. Division")
 
-
     choice = input("Enter choice (1/2/3/4): ")
 
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
 
-    if choice == '1':
+    if choice == "1":
         print("Result:", add(num1, num2))
-    elif choice == '2':
+    elif choice == "2":
         print("Result:", subtract(num1, num2))
-    elif choice == '3':
+    elif choice == "3":
         print("Result:", multiply(num1, num2))
-    elif choice == '4':
+    elif choice == "4":
         print("Result:", divide(num1, num2))
     else:
         print("Invalid choice")
+
 
 if __name__ == "__main__":
     main()
