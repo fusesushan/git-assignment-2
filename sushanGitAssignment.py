03af1b5cf3e188c373bf2e6589727401c5cca088
fusesushan <sushan.kattel@fusemachines.com>
2023-08-03 13:40:03 +0545

    feat:Adding commit message hook

--- sushanGitAssignment.py
+++ sushanGitAssignment.py
@@ -30,13 +30,13 @@ def main():
     num2 = float(input("Enter second number: "))
 
     if choice == "1":
-        print("Result:", add(num1, num2))
+        print("The Result:", add(num1, num2))
     elif choice == "2":
-        print("Result:", subtract(num1, num2))
+        print("The Result:", subtract(num1, num2))
     elif choice == "3":
-        print("Result:", multiply(num1, num2))
+        print("The Result:", multiply(num1, num2))
     elif choice == "4":
-        print("Result:", divide(num1, num2))
+        print("The Result:", divide(num1, num2))
     else:
         print("Invalid choice")
 
